ae99503bcb8e48f5109841209349066c29a4d3cf
KG <kilian_kegel@hotmail.com>
2020-07-18 17:57:14 +0200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository universe -y
+sudo apt-get update -y
+sudo apt-get install --fix-missing -y
+sudo apt update -y
+sudo apt-get upgrade -y
+sudo apt-get install gawk -y
+sudo apt-get install wget -y
+sudo apt-get install git-core -y
+sudo apt-get install diffstat -y
+sudo apt-get install unzip -y
+sudo apt-get install texinfo  -y
+sudo apt-get install install-info info -y
+sudo apt-get install gcc-multilib -y
+sudo apt-get install build-essential -y
+sudo apt-get install chrpath -y
+sudo apt-get install socat -y
+sudo apt-get install cpio -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-pexpect -y
+sudo apt-get install python3-git -y
+sudo apt-get install python3-jinja2 -y
+sudo apt-get install python3-distutils -y
+sudo apt-get install xz-utils -y
+sudo apt-get install debianutils -y
+sudo apt-get install iputils-ping -y
+sudo apt-get install libegl1-mesa -y
+sudo apt-get install libsdl1.2-dev -y
+sudo apt-get install pylint3 -y
+sudo apt-get install xterm -y
+sudo apt install chrpath -y
+sudo apt install libncurses5-dev -y
+sudo apt-get install python3-distutils -y
+sudo dpkg-reconfigure dash -y
